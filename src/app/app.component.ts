71370248b907167380c5d9108cfcf77822efed9e
Aleksandra Nikolaieva <oleksandranikolaieva@gmail.com>
2019-05-17 02:24:55 +0300

    code style corrections following lint rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,11 @@ export class AppComponent {
         id: 1,
         name: 'Name Loooooooooooooooong',
         imgSrc: '../assets/images/defUser.png'
-    }
-    isUserMenuOpen: boolean = false;
-    isNavMenuOpen: boolean = false;
-    isSearchOpen: boolean = false;
-    isAuxOpen: boolean = true;
+    };
+    isUserMenuOpen = false;
+    isNavMenuOpen = false;
+    isSearchOpen = false;
+    isAuxOpen = true;
     menuItems: Array<MenuItem> = [
         {
             title: 'Dashboard',
@@ -56,7 +56,7 @@ export class AppComponent {
             author: 'David Gilbertson',
             date: '2018-05-20',
             description: 'Most elements don’t have any interesting methods, so unless you actually sift through the spec for things you’ll almost never use, it’s easy to miss the little nuggets that are scattered throughout.',
-            img:'../assets/images/html1.jpg'
+            img: '../assets/images/html1.jpg'
         },
         {
             id: 4,
@@ -64,7 +64,7 @@ export class AppComponent {
             author: 'Antonio Regadas',
             date: '2016-12-22',
             description: 'Clean and organize HTML, that’s what we as Front-end Developers always aim for. Well with Pug, formerly known as “Jade” (a registered trademark, and as a result a rename was needed) it’s a high performance and feature-rich templating engine that’s easy to achieve. Simply put, Pug is a clean, white space/indentation sensitive syntax for writing html.',
-            img:'../assets/images/pug1.png'
+            img: '../assets/images/pug1.png'
         },
         {
             id: 5,
@@ -72,15 +72,15 @@ export class AppComponent {
             author: 'Fatos Morina',
             date: '2019-01-17',
             description: 'In this tutorial Sassy, Sass and SCSS will refer to roughly the same thing. Conceptually, there isn’t much difference. You will learn the difference as you learn more, but basically SCSS is the one most people use now. It’s just a more recent (and according to some, superior) version of the original Sass syntax.',
-            img:'../assets/images/scss1.png'
+            img: '../assets/images/scss1.png'
         },
         {
             id: 6,
             title: '10 Interview Questions Every JavaScript Developer Should Know',
             author: 'Eric Elliott',
             date: '2019-04-08',
-            description: 'At most companies, management must trust the developers to give technical interviews in order to assess candidate skills. If you do well as a candidate, you’ll eventually need to interview. Here’s how.',
-            img:'../assets/images/js1.png'
+            description: `At most companies, management must trust the developers to give technical interviews in order to assess candidate skills. If you do well as a candidate, you’ll eventually need to interview. Here’s how.`,
+            img: '../assets/images/js1.png'
         }
     ];
 
@@ -92,7 +92,7 @@ export class AppComponent {
 
     private doSearch(forSearch: string): void {
         console.log(forSearch);
-        //do search
+        // do search
     }
 
     private toggleNavMenu(): void {
--- src/app/models.ts
+++ src/app/models.ts
@@ -1,19 +1,19 @@
 export interface User {
-    id: number,
-    name: string,
-    imgSrc: string
+    id: number;
+    name: string;
+    imgSrc: string;
 }
 
 export interface MenuItem {
-    title: string,
-    icon: string
+    title: string;
+    icon: string;
 }
 
- export interface Article {
-    id: number,
-    title: string,
-    author: string,
-    date: string,
-    description: string,
-    img: string
+export interface Article {
+    id: number;
+    title: string;
+    author: string;
+    date: string;
+    description: string;
+    img: string;
 }
