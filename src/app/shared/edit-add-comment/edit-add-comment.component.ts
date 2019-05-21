876c7920612b64e2c8012a87b4f7aee3008ca56b
Aleksandra Nikolaieva <oleksandranikolaieva@gmail.com>
2019-05-21 22:01:34 +0300

    add edit/add comment component

--- src/app/shared/edit-add-comment/edit-add-comment.component.ts
+++ src/app/shared/edit-add-comment/edit-add-comment.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Comment } from 'src/app/workshops/models';
 
 @Component({
     selector: 'app-edit-add-comment',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./edit-add-comment.component.scss']
 })
 export class EditAddCommentComponent implements OnInit {
+    @Input() comment: Comment;
+    @Output() changeComment = new EventEmitter<Comment>();
 
     constructor() { }
 
     ngOnInit() {
     }
 
+    private submit(text: string): void {
+        if (text) {
+            this.comment.text = text;
+            this.changeComment.emit(this.comment);
+        }
+    }
 }
--- src/app/workshops/workshop-page/workshop-page.component.ts
+++ src/app/workshops/workshop-page/workshop-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Article } from '../models';
+import { Article, Comment } from '../models';
 import { articles } from '../workshops';
+import { User } from 'src/app/core/models';
 
 @Component({
     selector: 'app-workshop-page',
@@ -10,6 +11,13 @@ import { articles } from '../workshops';
 })
 export class WorkshopPageComponent implements OnInit {
     article: Article;
+    loggedUser: User = {id: 10, name: 'Ivan Pupkin', imgSrc: ''};
+    newComment: Comment = {
+        author: this.loggedUser,
+        text: '',
+        date: new Date()
+    };
+    editorNumber: number;
     constructor(private activateRoute: ActivatedRoute) { }
 
     ngOnInit() {
@@ -18,8 +26,30 @@ export class WorkshopPageComponent implements OnInit {
         });
     }
 
-    private changeLikeHandler(to: boolean) {
+    private changeLikeHandler(to: boolean): void {
         console.log('workshop page like', to);
         // send changes to server
+    }
+
+    private addComment(newComment: Comment): void {
+        this.article.comments.push(newComment);
+        this.newComment = {
+            author: this.loggedUser,
+            text: '',
+            date: new Date()
+        };
+    }
+
+    private editComment(comment: Comment, index: number): void {
+        this.article.comments[index] = comment;
+        this.editorNumber = null;
+    }
+
+    private deleteComment(index: number) {
+        this.article.comments.splice(index, 1);
+    }
+
+    private openEditor(index: number) {
+        this.editorNumber = index;
     }
 }
