415a8785ebfa51cde590a8b96d9953266c402c67
Aleksandra Nikolaieva <oleksandranikolaieva@gmail.com>
2019-06-03 19:23:25 +0300

    fix to-top-button visibility

--- src/app/shared/to-top-button/to-top-button.component.ts
+++ src/app/shared/to-top-button/to-top-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ElementRef, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, Renderer2, ChangeDetectorRef, OnDestroy, NgZone } from '@angular/core';
 
 @Component({
     selector: 'app-to-top-button',
@@ -6,14 +6,35 @@ import { Component, OnInit, Input, ElementRef, ChangeDetectionStrategy } from '@
     styleUrls: ['./to-top-button.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ToTopButtonComponent implements OnInit {
+export class ToTopButtonComponent implements OnInit, OnDestroy {
     @Input() hostElement: HTMLElement;
-    constructor() { }
+    isVisible = false;
+    listener: Function;
+    constructor(private renderer: Renderer2,
+        private cdr: ChangeDetectorRef,
+        private ngZone: NgZone) { }
 
     ngOnInit() {
+        const heightForVisibleButton = 20;
+        this.ngZone.runOutsideAngular(() => {
+            this.listener = this.renderer.listen(this.hostElement, 'scroll', () => {
+                if (this.hostElement.scrollTop > heightForVisibleButton) {
+                    this.isVisible = true;
+                    this.cdr.detectChanges();
+                } else {
+                    this.isVisible = false;
+                    this.cdr.detectChanges();
+                }
+            });
+        });
     }
 
     goToTop(): void {
         this.hostElement.scrollTo({left: 0, top: 0, behavior: 'smooth'});
+        this.isVisible = false;
+    }
+
+    ngOnDestroy(): void {
+        this.listener();
     }
 }
