e2074b5bc6f595c68ba817c151060019a69d136d
Aleksandra Nikolaieva <oleksandranikolaieva@gmail.com>
2019-06-03 07:12:37 +0300

    change not found

--- src/app/core/not-found/not-found.component.ts
+++ src/app/core/not-found/not-found.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, NgZone, AfterViewInit } from '@angular/core';
 
 @Component({
     selector: 'app-not-found',
     templateUrl: './not-found.component.pug',
     styleUrls: ['./not-found.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent implements OnInit, AfterViewInit {
+    error = '';
 
-    constructor() { }
+    constructor(private cdr: ChangeDetectorRef, private ngZone: NgZone) { }
 
     ngOnInit() {
     }
 
+    ngAfterViewInit(): void {
+        const toShow = '404, page not found.';
+        let i = 0;
+        this.ngZone.runOutsideAngular(() => {
+            const interval = setInterval(() => {
+                this.error += toShow[i];
+                this.cdr.detectChanges();
+                i++;
+                if (i === toShow.length) {
+                    clearInterval(interval);
+                }
+            }, 200);
+        });
+    }
 }
