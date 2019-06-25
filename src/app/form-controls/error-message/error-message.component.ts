9f1e57194f03647b21a0ff5862d7040fe09edd2c
Aleksandra Nikolaieva <oleksandranikolaieva@gmail.com>
2019-06-25 05:03:33 +0300

    add error messages for constructor

---
+++ src/app/form-controls/error-message/error-message.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input, ChangeDetectorRef } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+    selector: 'app-error-message',
+    templateUrl: './error-message.component.pug',
+    styleUrls: ['./error-message.component.scss'],
+    //changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ErrorMessageComponent implements OnInit {
+
+    @Input() field: FormControl;
+    errorsConfig: any = {
+        required: 'Field is required',
+        answerChoice: `The correct answer is not selected`
+    };
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    get errors() {
+        const errors: Array<string> = [];
+        if (!this.field || !this.field.errors) {
+            return null;
+        }
+        if (this.field.errors.hasOwnProperty('minlength')) {
+            this.errorsConfig.minlength = `Minimun length is ${this.field.errors.minlength.requiredLength}`;
+        }
+        if (this.field.errors.hasOwnProperty('maxlength')) {
+            this.errorsConfig.maxlength = `Maximum length is ${this.field.errors.maxlength.requiredLength}`;
+        }
+        if (this.field.errors.hasOwnProperty('arrLength')) {
+            this.errorsConfig.arrLength = `Should be at least ${this.field.errors.arrLength.minLength} item`;
+        }
+        if (this.field.errors) {
+            Object.keys(this.field.errors).forEach(errType => {
+                errors.push(this.errorsConfig[errType]);
+            });
+        }
+        return errors;
+    }
+}
